4690544f932df41b372828c0002631161d031aab
Troy Warr <troy3000@gmail.com>
2019-07-04 01:40:23 -0500

    Updates

---
+++ precache-manifest.ddd08e5fe418230b97b9790f4f97dcde.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ff52b9fb4e8bf55276207583080546",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "84740c8722f9ad2028d4",
+    "url": "/demo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d5afee2ecb5afb7ed09e",
+    "url": "/demo/static/css/main.346d8ea5.chunk.css"
+  },
+  {
+    "revision": "84740c8722f9ad2028d4",
+    "url": "/demo/static/js/2.0922f16a.chunk.js"
+  },
+  {
+    "revision": "d5afee2ecb5afb7ed09e",
+    "url": "/demo/static/js/main.4d8511a2.chunk.js"
+  },
+  {
+    "revision": "2f23047b078009b94f5f",
+    "url": "/demo/static/js/runtime~main.96f7b30f.js"
+  },
+  {
+    "revision": "4895748922496daaf1dcf859b40d0967",
+    "url": "/demo/static/media/Adelle Bold Italic.48957489.woff2"
+  },
+  {
+    "revision": "a99d86b3fe979e235cf204cf25801523",
+    "url": "/demo/static/media/Adelle Bold Italic.a99d86b3.woff"
+  },
+  {
+    "revision": "07bc24ac469eda017b45990abda015b4",
+    "url": "/demo/static/media/Adelle Bold Regular.07bc24ac.woff2"
+  },
+  {
+    "revision": "96f9e8a7bb62125857f169375225898e",
+    "url": "/demo/static/media/Adelle Bold Regular.96f9e8a7.woff"
+  },
+  {
+    "revision": "8bb711abeff288b5a3fdc9d541124f3d",
+    "url": "/demo/static/media/Adelle Extra Bold Italic.8bb711ab.woff2"
+  },
+  {
+    "revision": "92edf7bea29a6b38edf43bae4e44e29f",
+    "url": "/demo/static/media/Adelle Extra Bold Italic.92edf7be.woff"
+  },
+  {
+    "revision": "59b6e2882feab533153b9310001de13f",
+    "url": "/demo/static/media/Adelle Extra Bold Regular.59b6e288.woff2"
+  },
+  {
+    "revision": "7a4aa82dd25cd56bded0639be5c64b83",
+    "url": "/demo/static/media/Adelle Extra Bold Regular.7a4aa82d.woff"
+  },
+  {
+    "revision": "50ca1e73ac524b53bf8eca79042d1306",
+    "url": "/demo/static/media/Adelle Heavy Italic.50ca1e73.woff"
+  },
+  {
+    "revision": "afc6f3af55915f81a28d7d9b0b4f054a",
+    "url": "/demo/static/media/Adelle Heavy Italic.afc6f3af.woff2"
+  },
+  {
+    "revision": "5b1246ee34ac31c22ef94e863e8f9beb",
+    "url": "/demo/static/media/Adelle Heavy Regular.5b1246ee.woff"
+  },
+  {
+    "revision": "ae9de7fb796b162988b208bed3ce035c",
+    "url": "/demo/static/media/Adelle Heavy Regular.ae9de7fb.woff2"
+  },
+  {
+    "revision": "67cd1cd383c0c42e830cc4fec95ab316",
+    "url": "/demo/static/media/Adelle Light Italic.67cd1cd3.woff"
+  },
+  {
+    "revision": "e492e00b1e5f8f019dc2289726798f81",
+    "url": "/demo/static/media/Adelle Light Italic.e492e00b.woff2"
+  },
+  {
+    "revision": "d32f4c9552b353ce251c19227f62e484",
+    "url": "/demo/static/media/Adelle Light Regular.d32f4c95.woff2"
+  },
+  {
+    "revision": "fa6ee4016af8dac1a806c4fd0665babe",
+    "url": "/demo/static/media/Adelle Light Regular.fa6ee401.woff"
+  },
+  {
+    "revision": "4bdb66c60dec3bde772267d049dfafc4",
+    "url": "/demo/static/media/Adelle Normal Regular.4bdb66c6.woff"
+  },
+  {
+    "revision": "88ae8936b8dee49d5c39d366035dbc21",
+    "url": "/demo/static/media/Adelle Normal Regular.88ae8936.woff2"
+  },
+  {
+    "revision": "38714b7ac319f3f27e0dc586614ab478",
+    "url": "/demo/static/media/Adelle Regular Italic.38714b7a.woff"
+  },
+  {
+    "revision": "f642fd4328619692cf250847d9786e8f",
+    "url": "/demo/static/media/Adelle Regular Italic.f642fd43.woff2"
+  },
+  {
+    "revision": "25b2cd6e35146f5c7cb9630a681d48b5",
+    "url": "/demo/static/media/Adelle Semi Bold Italic.25b2cd6e.woff2"
+  },
+  {
+    "revision": "dfdb80ba131971b310c55a2b628aa405",
+    "url": "/demo/static/media/Adelle Semi Bold Italic.dfdb80ba.woff"
+  },
+  {
+    "revision": "5d3237f0910340e53704b66624e36efe",
+    "url": "/demo/static/media/Adelle Semi Bold Regular.5d3237f0.woff"
+  },
+  {
+    "revision": "ba40ea931152512734ec27f28439b75c",
+    "url": "/demo/static/media/Adelle Semi Bold Regular.ba40ea93.woff2"
+  },
+  {
+    "revision": "76981334cc923d430cdc822501f6553f",
+    "url": "/demo/static/media/Brown Pro Bold Italic.76981334.woff"
+  },
+  {
+    "revision": "7e878293ee3ecafbc70cf56bd96f089d",
+    "url": "/demo/static/media/Brown Pro Bold Italic.7e878293.woff2"
+  },
+  {
+    "revision": "692d237bdef7a975f1ede9918d9a84f3",
+    "url": "/demo/static/media/Brown Pro Bold Regular.692d237b.woff"
+  },
+  {
+    "revision": "b8e67fde7e93d6ab84165252770c899a",
+    "url": "/demo/static/media/Brown Pro Bold Regular.b8e67fde.woff2"
+  },
+  {
+    "revision": "60982989bc9f9467d7a603acfe7ee588",
+    "url": "/demo/static/media/Brown Pro Light Italic.60982989.woff"
+  },
+  {
+    "revision": "7e7c47acff7f9bb894616ec67366b97c",
+    "url": "/demo/static/media/Brown Pro Light Italic.7e7c47ac.woff2"
+  },
+  {
+    "revision": "98ebb4635526c9df32b9ad3c7fdacb7e",
+    "url": "/demo/static/media/Brown Pro Light Regular.98ebb463.woff2"
+  },
+  {
+    "revision": "ad13e4c416a59b5bd8ca006670be0e13",
+    "url": "/demo/static/media/Brown Pro Light Regular.ad13e4c4.woff"
+  },
+  {
+    "revision": "2c4ce00a4fc8ddf14cd6cf9cc1b9e21e",
+    "url": "/demo/static/media/Brown Pro Normal Italic.2c4ce00a.woff2"
+  },
+  {
+    "revision": "9ff41bbea15b4be060a4347f7b51514a",
+    "url": "/demo/static/media/Brown Pro Normal Italic.9ff41bbe.woff"
+  },
+  {
+    "revision": "7a5def5ba9ecf16833c08d6f29b71685",
+    "url": "/demo/static/media/Brown Pro Normal Regular.7a5def5b.woff"
+  },
+  {
+    "revision": "a5a9771aca7c4dae3234c4760cbf4613",
+    "url": "/demo/static/media/Brown Pro Normal Regular.a5a9771a.woff2"
+  },
+  {
+    "revision": "75fae787c001ce5cce2a3531326e39c6",
+    "url": "/demo/static/media/Brown Pro Thin Italic.75fae787.woff"
+  },
+  {
+    "revision": "d24dc9a21ed6159b5abe0133a4ca537d",
+    "url": "/demo/static/media/Brown Pro Thin Italic.d24dc9a2.woff2"
+  },
+  {
+    "revision": "323390cea64ec6bf9d0bb53e49f3062c",
+    "url": "/demo/static/media/Brown Pro Thin Regular.323390ce.woff2"
+  },
+  {
+    "revision": "4d4802ed1fced51040fa7a6f7376fca4",
+    "url": "/demo/static/media/Brown Pro Thin Regular.4d4802ed.woff"
+  },
+  {
+    "revision": "69b6fe793d90f8794413f53d5be307c3",
+    "url": "/demo/static/media/masthead-background.69b6fe79.jpg"
+  }
+]);
